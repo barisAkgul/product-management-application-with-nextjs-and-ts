8e94d481ddf14645220603e413b851b8b3eaf2dd
barisAkgul <barisakgul1994@gmail.com>
2023-09-21 23:01:21 +0300

    Added Logo component

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./logo.scss";
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <span>W.</span>
+    </div>
+  );
+};
+
+export default Logo;
